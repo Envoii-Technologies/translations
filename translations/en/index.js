4c4c7e2f0677d5c60c2bfdc016d8adedfc9377b2
FRickReich <frickreich@gmail.com>
2023-11-17 16:37:54 +0100

    feat: add LoadingIndicator component translations

--- translations/en/index.js
+++ translations/en/index.js
@@ -1,10 +1,12 @@
 import MainMenu from './MainMenu';
 import MediaPage from './MediaPage';
 import CardViewer from './CardViewer';
+import LoadingIndicator from './LoadingIndicator';
 
 export default
 {
     MainMenu: {...MainMenu},
     MediaPage: {...MediaPage},
     CardViewer: {...CardViewer},
+    LoadingIndicator: {...LoadingIndicator}
 }
